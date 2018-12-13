4930a6179a255e5b3b9643a0d7bc1be928599a02
MissTizz <32457342+MissTizz@users.noreply.github.com>
2018-12-13 02:04:16 +0100

    Design created

--- src/app/trophy-room/trophy-room.component.ts
+++ src/app/trophy-room/trophy-room.component.ts
@@ -9,22 +9,6 @@ import {User} from '../shared/models/User';
 export class TrophyRoomComponent implements OnInit {
 
   users: User[];
-  /*
-  users: User[] = [{id: 1, twitchname: 'MissTizz', role: 'Queen',
-  wins: 99, losses: 0, kills: 83573422, deaths: 0, assists: 5,
-  kda: 99.2, winLoss: 100, point: 999999, rank: 1},
-  {id: 2, twitchname: 'MissPizz', role: 'Queen',
-    wins: 99, losses: 0, kills: 83573422, deaths: 0, assists: 5,
-    kda: 99.2, winLoss: 100, point: 999999, rank: 2},
-    {id: 3, twitchname: 'MissFizz', role: 'Queen',
-      wins: 99, losses: 0, kills: 83573422, deaths: 0, assists: 5,
-      kda: 99.2, winLoss: 100, point: 999999, rank: 3},
-    {id: 4, twitchname: 'MissKizz', role: 'Queen',
-      wins: 99, losses: 0, kills: 83573422, deaths: 0, assists: 5,
-      kda: 99.2, winLoss: 100, point: 999999, rank: 4},
-    {id: 5, twitchname: 'MissLort', role: 'Queen',
-      wins: 99, losses: 0, kills: 83573422, deaths: 0, assists: 5,
-      kda: 99.2, winLoss: 100, point: 999999, rank: 5}]*/
 
   constructor() {}
 
