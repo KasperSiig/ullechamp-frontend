901cc31e6115dc6dfdbb8920791c37df8a9b2151
MissTizz <32457342+MissTizz@users.noreply.github.com>
2018-12-13 21:31:32 +0100

    Add to Queue
    When button "tilmeld" is clicked, the user will be added to queue and page refreshed

--- src/app/tournament/tournament.component.ts
+++ src/app/tournament/tournament.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../shared/models/User';
 import {LeaderboardService} from '../shared/services/leaderboard.service';
+import {TournamentService} from '../shared/services/tournament.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-tournament',
@@ -11,13 +13,22 @@ export class TournamentComponent implements OnInit {
 
   users: User[];
 
-  constructor(private leaderboardService: LeaderboardService) { }
+  constructor(private tournamentService: TournamentService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    this.leaderboardService.getUsers()
+    this.tournamentService.getQueue()
       .subscribe(users => {
         this.users = users;
         this.users.reverse();
+      });
+  }
+
+  signUp() {
+    this.tournamentService.signUp()
+      .subscribe(() => {
+        location.reload(true);
       });
   }
 
